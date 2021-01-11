e6e873fdbfaa955948d425ab8298d8d3c765d9b2
anthony <anthonytran.dev@gmail.com>
2021-01-11 05:26:37 +0000

    basic router implemented

--- src/App.svelte
+++ src/App.svelte
@@ -1,51 +1,15 @@
 <!-- App.svelte -->
 <script>
-  import { onMount } from 'svelte';
-  let count = 0; // @hmr:keep
-  onMount(() => {
-    const interval = setInterval(() => count++, 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  });
+  import { Router, Route } from "svelte-routing";
+
+  import About from './pages/About.svelte'
+  import Blog from './pages/Blog.svelte'
+  import Home from './pages/Home.svelte'
+
+  export let url = "";
 </script>
-<style>
-  .App-header {
-    background-color: #f9f6f6;
-    color: #333;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-  }
-  .App-logo {
-    height: 36vmin;
-    pointer-events: none;
-    margin-bottom: 3rem;
-    animation: App-logo-pulse infinite 1.6s ease-in-out alternate;
-  }
-  @keyframes App-logo-pulse {
-    from {
-      transform: scale(1);
-    }
-    to {
-      transform: scale(1.06);
-    }
-  }
-</style>
-<div class="App">
-  <header class="App-header">
-    <img src="/logo.svg" class="App-logo" alt="logo" />
-    <p>Page has been open for <code>{count}</code> seconds.</p>
-    <a
-    class="App-link"
-    href="https://svelte.dev"
-    target="_blank"
-    rel="noopener noreferrer"
-    >
-      Learn Svelte
-    </a>
-  </header>
-</div>
+<Router url="{url}">
+  <Route path="blog" component="{Blog}" />
+  <Route path="about" component="{About}" />
+  <Route path="/"><Home /></Route>
+</Router>
