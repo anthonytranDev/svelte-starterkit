d731fee8dc0bad4326f8a60952b178dea5954727
anthony <anthonytran.dev@gmail.com>
2021-01-11 08:00:52 +0000

    add basic eslint support for project setup
    eslint decisions - babel was required to use latest
    ECMAScript versions with eslint

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+// babel is only used to remove eslint errors, in regards to `import.meta`
+const presets = [
+  [
+    "@babel/env",
+    {
+      targets: {
+        edge: "17",
+        firefox: "60",
+        chrome: "67",
+        safari: "11.1",
+      },
+      useBuiltIns: "usage",
+      corejs: "3.6.4",
+    },
+  ],
+]
+
+module.exports = { presets }
