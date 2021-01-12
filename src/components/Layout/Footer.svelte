f8995f5f14e3908f2a612f6bd7b0039aebca3ad0
anthony <anthonytran.dev@gmail.com>
2021-01-12 02:53:46 +0000

    footer init

---
+++ src/components/Layout/Footer.svelte
@@ -0,0 +1,37 @@
+<style>
+  .footer {
+    padding: 1rem 0.75rem;
+  }
+
+  .footer-wrapper {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .footer-license {
+    font-weight: 700;
+  }
+
+  #github-logo {
+    position: relative;
+    top: 3px;
+    height: 20px;
+  }
+</style>
+
+<div class="footer">
+  <div class="footer-wrapper">
+    <div class="footer-license">MIT License</div>
+    <div>
+      <a href="https://github.com/anthonytranDev">
+        <img
+          id="github-logo"
+          src="/github-logo.svg"
+          class="home-logo"
+          alt="logo"
+        />
+        <span>GitHub: anthonytranDev/svelte-basic-starter</span>
+      </a>
+    </div>
+  </div>
+</div>
