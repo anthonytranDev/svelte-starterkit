011e0ea102384ea5add8f561ba1cdea9d67928c4
anthony <anthonytran.dev@gmail.com>
2021-01-12 02:55:15 +0000

    wrap routes with layout

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,8 @@
 <script>
   import { Router, Route } from "svelte-routing"
 
+  import Layout from './components/Layout/index.svelte'
+
   import About from "./pages/About.svelte"
   import Blog from "./pages/Blog/index.svelte"
   import BlogPost from "./pages/Blog/BlogPost.svelte"
@@ -10,8 +12,10 @@
 </script>
 
 <Router>
-  <Route path="/"><Home /></Route>
-  <Route path="about" component="{About}" />
-  <Route path="blog" component="{Blog}" />
-  <Route path="blog/:id" component="{BlogPost}" />
+  <Layout>
+    <Route path="/"><Home /></Route>
+    <Route path="about" component="{About}" />
+    <Route path="blog" component="{Blog}" />
+    <Route path="blog/:id" component="{BlogPost}" />
+  </Layout>
 </Router>
