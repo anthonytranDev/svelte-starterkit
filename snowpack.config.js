d0525ed1387fc56f9a2302fe93ba0bb83c941dcf
anthony <anthonytran.dev@gmail.com>
2021-01-12 00:09:49 +0000

    decouple tests files when building with snowpack

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,27 +1,9 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    // directory name: 'build directory'
     public: "/",
     src: "/dist",
   },
   plugins: ["@snowpack/plugin-svelte"],
-  install: [
-    /* ... */
-  ],
-  installOptions: {
-    /* ... */
-  },
-  devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
-  proxy: {
-    /* ... */
-  },
-  alias: {
-    /* ... */
-  },
+  exclude: ["**/__tests__/**"],
 }
